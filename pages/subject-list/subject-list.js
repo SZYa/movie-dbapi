e44ac8ce7afacdd64778de59522b0a90654daf74
SZYa <1850068618@qq.com>
2018-03-28 15:52:32 +0800

    first commit

---
+++ pages/subject-list/subject-list.js
@@ -0,0 +1,46 @@
+import cfg from '../../common/config/index.js'
+Page({
+  data: {
+    movies: [],
+    page: 1,
+    size: 6,
+    loading: true,
+    type:''
+  },
+  onLoad(options) {
+    const {type} = options
+    this.setData({ type })
+    this.loadMovies()
+  },
+  loadMovies() {
+    const { size, page, type } = this.data;
+    this.setData({ loading: true });
+    wx.request({
+      url: `${cfg.domain}/list?type=${type}&page=${page}&size=${size}`,
+      success: (res) => {
+        const { data } = res.data
+        const movies = this.data.movies
+        for (let i = 0; i < data.length; i += 2) {
+          movies.push([data[i], data[i + 1] ? data[i + 1] : null])
+        }
+        this.setData({
+          movies,
+          loading: false 
+        })
+      }
+    })
+  },
+  scrollHandler() {
+    const { page } = this.data
+    this.setData({
+      page: page + 1
+    })
+    this.loadMovies()
+  },
+  gotoDetailHandler(e) {
+    const { id } = e.currentTarget.dataset
+    wx.navigateTo({
+      url: '../movie-detail/movie-detail?id=' + id,
+    })
+  }
+})
---
+++ pages/user/user.js
@@ -0,0 +1,17 @@
+// pages/user/user.js
+Page({
+  data:{
+    avatarUrl:'',
+    nickName:'未知'
+  },
+  onLoad(){
+    wx.getUserInfo({
+      success:(res)=>{
+        this.setData({
+          avatarUrl:res.userInfo.avatarUrl,
+          nickName:res.userInfo.nickName
+        })
+      }
+    })
+  }
+})
